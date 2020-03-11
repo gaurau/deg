42d8c6f792ed39613d44d9a195b1cad397601c5c
gaurau <pawanupadhyay70@gmail.com>
2020-03-11 15:11:17 -0500

    DEGs

--- file.R
+++ file.R
@@ -1,1 +1,15 @@
-print("file.txt")
+FROM bioconductor/bioconductor_docker:RELEASE_3_10
+
+RUN apt-get update && apt-get install -y \
+    r-cran-xml \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    libxml2-dev \
+    ghostscript
+ENV PATH=pkg-config:$PATH
+
+RUN install2.r --error --deps TRUE \
+    doParallel \
+    && rm -rf /tmp/downloaded_packages/
+
+RUN Rscript -e 'BiocManager::install(c("edgeR", "biomaRt", "limma", "DESeq2", "tidyverse"))'
